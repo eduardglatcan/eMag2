69305476d1d65805e20e4153c09c37d74fd1b265
Latcan Eduard <36689756+eduardglatcan@users.noreply.github.com>
2019-01-07 22:37:33 +0200

    Add project files.

---
+++ eMag2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="eMag2.MvcApplication" Language="C#" %>
---
+++ eMag2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(eMag2.Startup))]
+namespace eMag2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
